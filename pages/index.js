1b70df6b066cb82487a7efb55e13c2a8f1e3b626
dylangangat <dylan.gangat@gmail.com>
2022-09-08 13:18:07 +0200

    Replaced font awesome icons with inline svg icons

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,6 @@ import Head from "next/head";
 import { format } from "date-fns";
 import { motion } from "framer-motion";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-
 import Header from "../components/Header";
 import SectionCurrentWeather from "../components/SectionCurrentWeather";
 import SectionFutureWeather from "../components/SectionFutureWeather";
@@ -153,7 +150,17 @@ export default function Home() {
                 className="container back"
                 onClick={() => setCityForecastWeather("")}
               >
-                <FontAwesomeIcon icon={faArrowLeft} className="fa-lg" />
+                <svg
+                  width="25"
+                  height="25"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path
+                    fill="#8C8AFF"
+                    d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
+                  />
+                </svg>
               </div>
 
               <SectionCurrentWeather
