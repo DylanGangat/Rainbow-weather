793bc969d8578bae1e188d1ee4adfc21d7dfc5cb
dylangangat <dylan.gangat@gmail.com>
2022-09-08 15:32:46 +0200

    Removed unecessary code

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,3 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faRainbow } from "@fortawesome/free-solid-svg-icons";
-// import { faPlus } from "@fortawesome/free-solid-svg-icons";
-
 import styles from "../styles/components/Header.module.scss";
 
 export default function Header({ cityForecastWeather }) {
@@ -10,8 +6,6 @@ export default function Header({ cityForecastWeather }) {
       {cityForecastWeather ? (
         <header className={styles["primary-header"]}>
           <div className="container">
-            {/* <FontAwesomeIcon icon={faRainbow} className="fa-xl" />{" "}
-            <FontAwesomeIcon icon={faPlus} /> */}
             <svg
               width="35"
               height="35"
@@ -41,8 +35,6 @@ export default function Header({ cityForecastWeather }) {
                 fill="#8C8AFF"
               />
             </svg>
-            {/* <FontAwesomeIcon icon={faRainbow} className=" fa-xl" />{" "}
-            <FontAwesomeIcon icon={faPlus} /> */}
           </div>
         </header>
       )}
